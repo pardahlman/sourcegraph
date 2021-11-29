6a0349e05d374c43743a56348070494f0c1ee01e
Vova Kulikov <vova@sourcegraph.com>
2021-11-29 18:55:18 +0300

    Align legend block layout with designs

--- client/web/src/enterprise/insights/components/insights-view-grid/components/backend-insight/BackendInsight.tsx
+++ client/web/src/enterprise/insights/components/insights-view-grid/components/backend-insight/BackendInsight.tsx
@@ -126,6 +126,8 @@ export const BackendInsight: React.FunctionComponent<BackendInsightProps> = prop
         return
     }
 
+    const chartLegendOrintation = insight.series.length > 3 ? 'horizontal' : 'vertical'
+
     return (
         <View.Root
             {...otherProps}
@@ -172,7 +174,7 @@ export const BackendInsight: React.FunctionComponent<BackendInsightProps> = prop
                 </View.ErrorContent>
             ) : (
                 data && (
-                    <LineChartSettingsContext.Provider value={{ zeroYAxisMin, layout: 'vertical' }}>
+                    <LineChartSettingsContext.Provider value={{ zeroYAxisMin, layout: chartLegendOrintation }}>
                         <View.Content
                             telemetryService={telemetryService}
                             content={data.view.content}
